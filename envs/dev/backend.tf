df28f28d9576a0741ca094f1271260bcb8d6539f
rakshi6361 <ksrakshith621@gmail.com>
2026-07-22 12:47:15 +0530

    config: update bucket name and github org for my accountt

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "zen-pharma-terraform-state-rakshi6361"  # Replace with your S3 bucket name
+    bucket       = "zen-pharma-terraform-state-rakshi63615"  # Replace with your S3 bucket name
     key          = "envs/dev/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
